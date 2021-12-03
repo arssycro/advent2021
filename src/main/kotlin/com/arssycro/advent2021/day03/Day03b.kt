330573828a7ed0340f28a60fc44ece146856adad
Josh Smith <josh.smith84@gmail.com>
2021-12-03 00:20:37 -0500

    Cleanup 3b

--- src/main/kotlin/com/arssycro/advent2021/day03/Day03b.kt
+++ src/main/kotlin/com/arssycro/advent2021/day03/Day03b.kt
@@ -11,36 +11,19 @@ class Day03b {
     fun process(){
         val numbers = readList("/day03.txt")
 
-        val oxygen = getOxygen(numbers)
-        val co2 = getCO2(numbers)
+        val oxygen = getValue(numbers, '0', '1')
+        val co2 = getValue(numbers, '1', '0')
 
         val oxygenNumber = Integer.parseInt(oxygen, 2)
         val co2Number = Integer.parseInt(co2, 2)
         println(oxygenNumber * co2Number)
     }
 
-    fun getOxygen(numbers: List<String>): String{
+    fun getValue(numbers: List<String>, gtValue: Char, lteValue: Char): String{
         var values = numbers
         for (i in 0..11){
             val count0 = values.map { it[i] }.count { it == '0' }
-            val match = if (count0 > (values.size/2)) '0' else '1'
-
-            values = values.filter { it[i] == match }
-            if (values.size == 1){
-                return values[0]
-            }
-        }
-
-        throw IllegalStateException()
-    }
-
-
-
-    fun getCO2(numbers: List<String>): String{
-        var values = numbers
-        for (i in 0..11){
-            val count0 = values.map { it[i] }.count { it == '0' }
-            val match = if (count0 <= (values.size/2)) '0' else '1'
+            val match = if (count0 > (values.size/2)) gtValue else lteValue
 
             values = values.filter { it[i] == match }
             if (values.size == 1){
